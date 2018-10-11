b2d4c1436f968e506856439df55885fd1a3bda6f
Óscar Díaz Domínguez <odiazdom@gmail.com>
2018-10-11 15:41:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "Gatos")
+option_b = os.getenv('OPTION_B', "Perros")
 redis_password = os.getenv('REDIS_PASSWORD', "Dogs")
 hostname = socket.gethostname()
 
